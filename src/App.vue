b296bbb0d6b19cc30929dd7a3359067868d658e1
Pyae Sone Paing <www.pyaesonekm2003@gmail.com>
2024-01-28 00:12:48 +0630

    create mini game project with vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>How Fast Can You Catch Me !</h1>
+  <button @click="play" :disabled="isPlaying">Click And Catch</button>
+  <div v-if="isPlaying">
+    <Block :delay="delay" @endGame="endGame"/>
+  </div>
+  <div v-if="end">
+    <Result :score="score" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue"
+import Result from "./components/Result.vue"
 
 export default {
   name: 'App',
+
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: 0,
+      end: false
+    }
+  },
+
+  methods: {
+    play() {
+      this.isPlaying = true,
+      this.delay = 2000+Math.random()*5000
+    },
+    endGame(score) {
+      this.score = score,
+      this.isPlaying = false,
+      this.end = true
+    }
+  },
+
   components: {
-    HelloWorld
+    Block,
+    Result
   }
 }
 </script>
---
+++ src/components/Block.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTime">Click Here</div>
+</template>
+
+<script>
+export default {
+    props:['delay'],
+    data(){
+        return {
+            showBlock:false,
+            score: 0,
+            timer: null
+        }
+    },
+
+    methods: {
+        startTime() {
+            this.timer = setInterval(()=>{
+                this.score+=50;
+            },50)
+        },
+        stopTime() {
+            clearInterval(this.timer)
+            this.$emit('endGame',this.score)
+        }
+    },
+
+    mounted() {
+        setTimeout(()=>{
+            this.showBlock = true;
+            this.startTime();
+        },this.delay)
+    },
+
+}
+</script>
+
+<style>
+    .block {
+        background-color: cyan;
+        width: 400px;
+        height: 300px;
+        margin: 40px auto;
+        padding-top: 20px;
+    }
+</style>
